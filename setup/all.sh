cf72179b3e2f4b76c62e4002e7d7f3a3ee6775bd
Paulware <Paulware@hotmail.com>
2020-05-12 07:56:09 -0500

    yo

--- setup/all.sh
+++ setup/all.sh
@@ -7,14 +7,14 @@ chmod 777 *.*
 cp /boot/runPython.sh /home/pi/Desktop/runPython.sh
 chmod 777 /home/pi/Desktop/runPython.sh
 apt-get update
+apt-get install bluetooth bluez blueman -y
+timedatectl set-timezone America/Chicago
 cd /boot
 ./ssh.sh
 ./keyboard.sh
-timedatectl set-timezone America/Chicago
 ./ap.sh
-pip install glob3
+# python3 already has glob3 # pip install glob3
 #./runMain.sh
-apt-get install bluetooth bluez blueman -y
 #ifup wlan0 
 echo "Done in setup, Using the wifi icon on desktop:turn wifi on, and set wifi country.  Then reboot"
 
