255b9fa21bae013540e5a4313365bc662273b6ce
Paul Richards <paulware@hotmail.com>
2022-10-02 22:24:03 -0500

    update

--- Uno.py
+++ Uno.py
@@ -28,9 +28,11 @@ class Uno ():
     def main (self):
        print ( 'Uno.main' )
        self.DISPLAYSURF.fill((BLACK))
+       displaySurface = self.DISPLAYSURF # refactor out?
+       
        pygame.display.set_caption('Play Uno')        
        
-       sprites = self.utilities.showImages (['quit.jpg'], [(400,500)] ) 
+       sprites = self.utilities.showImages (['quit.jpg'], [(400,600)] ) 
        pygame.display.update()       
 
        deck = Deck ('images/unoSpriteSheet.jpg', 10, 6, 52, 52)   
@@ -40,14 +42,26 @@ class Uno ():
           self.utilities.showStatus ( "Waiting for player to join")         
           pygame.display.update()
           self.comm.waitFor ( 'join uno')
-          hand = SubDeck (deck,7, displaySurface=self.DISPLAYSURF)
-          hand.draw()
-          opponent = SubDeck (deck,7, displaySurface=self.DISPLAYSURF)
-          #opponent.changeImage ( 'images/unoFlip.jpg')          
-          opponent.draw()
-          inputStack = SubDeck (deck, 52 - 14, displaySurface=self.DISPLAYSURF)
-          #inputStack.changeImage ( 'images/unoFlip.jpg')
-          inputStack.draw() # showStack (100,300,80,120,self.DISPLAYSURF)
+          deck        = Deck ('images/unoSpriteSheet.jpg', 10, 6, 52, 52)      
+          hand        = SubDeck (deck,  7, startXY=(100,400), displaySurface=displaySurface)   
+          discardPile = SubDeck (deck,  1, startXY=(100,200), displaySurface=displaySurface, xMultiplier=0.0, yMultiplier=0.0)
+          drawPile    = SubDeck (deck, 44, startXY=(300,200), displaySurface=displaySurface, xMultiplier=0.0, yMultiplier=0.0)
+          drawPile.hideAll () 
+          
+          # creates decks array 
+          cards=[]
+          cards.append (hand)
+          cards.append (drawPile)   
+          cards.append (discardPile)
+          decks = SubDecks (cards)    
+          
+          TextBox('Opponent', x=100, y=  5).draw()
+          TextBox('Discard',  x=100, y=175).draw()
+          TextBox('Draw',     x=310, y=175).draw()
+          TextBox('Hand',     x=100, y=375).draw()
+          
+          self.utilities.showStatus ( 'TBD: send cards to opponent' )
+          self.comm.send ( 'deal uno')
        else: # Host goes first...
           state = 2
           self.utilities.showStatus ( "Waiting for host to deal")
@@ -59,11 +73,13 @@ class Uno ():
        myMove = True 
        quit = False 
        while not self.utilities.quit and not self.gameOver() and not quit: 
-          if state == 1: 
+          decks.draw() # Show and set their x/y locations 
+          pygame.display.update()           
+          if state == 1: # I am host 
              if not self.comm.empty(): 
                 msg = self.comm.pop()
                 if msg.find ('join uno') > -1: 
-                   state = 3 
+                   state = 3
                    print ( 'found: join uno in ' + msg)
           elif state == 2: 
              if not self.comm.empty(): 
@@ -169,6 +185,10 @@ if __name__ == '__main__':
       quit = False
       
       comm.send ( 'join uno')   
+      comm.waitFor ( 'deal uno')      
+      
+      print ( 'Got deal uno')
+      print ( 'Waiting for cards' )
       
       while not quit: # len(deck.sprites) > 0:
          decks.draw() # Show and set their x/y locations
